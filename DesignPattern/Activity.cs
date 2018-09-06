aa53d65c97580ebaa0cb6825043005360e18a21b
Paarth Vashisht <pvashisht@tavisca.com>
2018-09-06 14:54:17 +0530

    Implemented the factory design,singleton and repository design using SOLID

---
+++ DesignPattern/Activity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class Activity:IProduct
+    {
+        public string name="SkyDive";
+       
+        public bool book = false;
+        public void Book()
+        {
+            book = true;
+            Logger.Instance.WriteLog("In activity Booking");
+            FileDataBase obj = new FileDataBase();
+            obj.WriteData(name, book);
+            Console.WriteLine("In activity booking");
+        }
+
+        public void Save()
+        {
+            Logger.Instance.WriteLog("In activity save ");
+            FileDataBase obj = new FileDataBase();
+            obj.WriteData(name, book);
+            Console.WriteLine("In activity save");
+        }
+    }
+}
